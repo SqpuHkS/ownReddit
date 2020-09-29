4662903b67cf57a263cb1a40ae6545932911f319
SqpuHkS <kovaleksey01@gmail.com>
2020-09-29 18:13:23 +0300

    add user_id to serializers

--- posts/serializers.py
+++ posts/serializers.py
@@ -5,7 +5,8 @@ from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.ReadOnlyField(source='user.id')
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'url', 'user', 'created']
+        fields = ['id', 'title', 'url', 'user', 'user_id', 'created']
